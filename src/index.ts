5beb6ba4f69c2e235af849bf49741c363bf4c82e
chenjianjx@gmail.com <chenjianjx@gmail.com>
2021-08-28 10:28:06 +1000

    temp submit before switching ide

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+
+console.log(argv['_']);
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-export class Main{
-    static log(val:any){
-        console.log(val);
-    }
-}
-Main.log('Hello World');
